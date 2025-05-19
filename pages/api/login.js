f5e533fd0b883aeecaf964b5074bb180b477d0af
Javierdz <javier@javierdz.me>
2025-05-19 22:22:37 +0200

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,23 +1,25 @@
+// /pages/api/login.js
 import axios from 'axios';
 
 export default async function handler(req, res) {
-  const CLIENT_ID = 'f1aeeee7-d115-4eab-8fb0-f8b50d57fd25';
-  const PASS_KEY = 'B7D0ED002990C2B5B1BCE474DDFE28A83A852B06A037063B8304B78307BEE93D8046A37C3BCC546AEA0E66152ABF3C625DFE642F0D78E79B4A0670252C17CFB2';
+  if (req.method !== 'GET') {
+    return res.status(405).json({ message: 'Only POST allowed' });
+  }
 
   try {
-    const response = await axios.get(
-      'https://openapi.emtmadrid.es/v2/mobilitylabs/user/login/',
-      {},
+    const { data } = await axios.get(
+      'https://openapi.emtmadrid.es/v1/mobilitylabs/user/login/',
+      {},  // body vacío según docs
       {
         headers: {
-          'X-ClientId': CLIENT_ID,
-          passKey: PASS_KEY,
+          'X-ClientId': 'f1aeeee7-d115-4eab-8fb0-f8b50d57fd25',
+          'passKey': 'B7D0ED002990C2B5B1BCE474DDFE28A83A852B06A037063B8304B78307BEE93D8046A37C3BCC546AEA0E66152ABF3C625DFE642F0D78E79B4A0670252C17CFB2',
+          'Content-Type': 'application/json',
         },
       }
     );
 
-    const token = response.data.data[0].accessToken;
-    res.status(200).json({ token });
+    res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
